3eca88e3d861d9589e71b898719df401177be5ff
Vitaliy Yurchenko <world-rom@yandex.ru>
2022-01-17 23:55:40 +0400

    Урок 21: добавление валидации форм

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import validation from './modules/validation';
 
 timer('19 january 2022 22:06:00');
 menu();
 modal();
+validation();
 //iAmTheSecond();
