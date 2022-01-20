f958ae07bce76a3242a6c2109f3e0f282520e152
Vitaliy Yurchenko <world-rom@yandex.ru>
2022-01-21 02:10:54 +0400

    Урок 23: создание слайдера

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import menu from './modules/menu';
 import modal from './modules/modal';
 import validation from './modules/validation';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
-timer('19 january 2022 22:06:00');
+timer('23 january 2022 22:06:00');
 menu();
 modal();
 validation();
 tabs();
+slider();
